561886de5aa5f07e3602dcfa3ca60bc57ed06ff4
jcmunav63 <jcmunav63@gmail.com>
2023-08-22 17:18:51 -0600

    Delete extra files and create Calculator file.

---
+++ src/Calculator.js
@@ -0,0 +1,27 @@
+function Calculator() {
+  return (
+  <div
+    className="calc">
+    <span className="display 4keys"></span>
+    <span className="1key">AC</span>
+    <span className="1key">+/-</span>
+    <span className="1key">%</span>
+    <span className="1key">&U+00F7;</span>
+    <span className="1key">7</span>
+    <span className="1key">8</span>
+    <span className="1key">9</span>
+    <span className="1key">*</span>
+    <span className="1key">4</span>
+    <span className="1key">5</span>
+    <span className="1key">6</span>
+    <span className="1key">-</span>
+    <span className="1key">1</span>
+    <span className="1key">2</span>
+    <span className="1key">3</span>
+    <span className="1key">+</span>
+    <span className="2keys">0</span>
+    <span className="1key">.</span>
+    <span className="1key">=</span>
+  </div>
+  );
+}
