8caac980567d66cdfeaeb4b2b17b2fdced6822f2
jcmunav63 <jcmunav63@gmail.com>
2023-08-23 19:37:24 -0600

    Add components directory and edit css file.

---
+++ src/components/Calculator.js
@@ -0,0 +1,28 @@
+function Calculator() {
+  return (
+    <div className="calc">
+      <span className="keys4">0</span>
+      <span className="key1">AC</span>
+      <span className="key1">+/-</span>
+      <span className="key1">%</span>
+      <span className="key1b">÷</span>
+      <span className="key1">7</span>
+      <span className="key1">8</span>
+      <span className="key1">9</span>
+      <span className="key1b">*</span>
+      <span className="key1">4</span>
+      <span className="key1">5</span>
+      <span className="key1">6</span>
+      <span className="key1b">-</span>
+      <span className="key1">1</span>
+      <span className="key1">2</span>
+      <span className="key1">3</span>
+      <span className="key1b">+</span>
+      <span className="keys2">0</span>
+      <span className="key1">.</span>
+      <span className="key1">=</span>
+    </div>
+  );
+}
+
+export default Calculator;
