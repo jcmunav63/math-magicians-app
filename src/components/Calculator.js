e4999823fdb6e8ca3828c6ed6182fda2217462ce
jcmunav63 <jcmunav63@gmail.com>
2023-08-24 21:27:56 -0600

    Edit Calculator component & css styles; add Button child component.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,26 +1,28 @@
+import Button from './Button';
+
 function Calculator() {
   return (
     <div className="calc">
       <span className="keys4">0</span>
-      <span className="key1">AC</span>
-      <span className="key1">+/-</span>
-      <span className="key1">%</span>
-      <span className="key1b">÷</span>
-      <span className="key1">7</span>
-      <span className="key1">8</span>
-      <span className="key1">9</span>
-      <span className="key1b">*</span>
-      <span className="key1">4</span>
-      <span className="key1">5</span>
-      <span className="key1">6</span>
-      <span className="key1b">-</span>
-      <span className="key1">1</span>
-      <span className="key1">2</span>
-      <span className="key1">3</span>
-      <span className="key1b">+</span>
-      <span className="keys2">0</span>
-      <span className="key1">.</span>
-      <span className="key1">=</span>
+      <Button text="AC" styling="key1" />
+      <Button text="+/-" styling="key1" />
+      <Button text="%" styling="key1" />
+      <Button text="÷" styling="key1b" />
+      <Button text="7" styling="key1" />
+      <Button text="8" styling="key1" />
+      <Button text="9" styling="key1" />
+      <Button text="*" styling="key1b" />
+      <Button text="4" styling="key1" />
+      <Button text="5" styling="key1" />
+      <Button text="6" styling="key1" />
+      <Button text="-" styling="key1b" />
+      <Button text="1" styling="key1" />
+      <Button text="2" styling="key1" />
+      <Button text="3" styling="key1" />
+      <Button text="+" styling="key1b" />
+      <Button text="0" styling="keys2" />
+      <Button text="." styling="key1" />
+      <Button text="=" styling="key1b" />
     </div>
   );
 }
