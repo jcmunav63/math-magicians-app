5be4d702de4e13a2e2725115473c01cbfff54163
jcmunav63 <jcmunav63@gmail.com>
2023-08-30 12:22:53 -0600

    Include sutraction and multiplication testing.

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,7 +1,7 @@
 import operate from './operate';
 describe('Testing math operations', () => {
 
-
+  // Sum testing
   test('Sum testing', () => {
 
     expect(operate(20, 30, '+')).toEqual('50');
@@ -9,6 +9,26 @@ describe('Testing math operations', () => {
     expect(operate(20, -30, '+')).toEqual('-10');
 
     expect(operate(-5, -10, '+')).toEqual('-15');
+  });
+
+  // Subtraction testing
+  test('Subtraction testing', () => {
+
+    expect(operate(20, 30, '-')).toEqual('-10');
+
+    expect(operate(20, -30, '-')).toEqual('50');
+
+    expect(operate(-5, -10, '-')).toEqual('5');
+  });
+
+   // Multiplication testing
+  test('Multiplication testing', () => {
+
+    expect(operate(20, 30, 'x')).toEqual('600');
+
+    expect(operate(20, -30, 'x')).toEqual('-600');
+
+    expect(operate(-5, -10, 'x')).toEqual('50');
   });
 
 }
